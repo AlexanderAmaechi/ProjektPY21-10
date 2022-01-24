0b55aed42a94221809e61d5ab0e962ef2b8a5e53
AlexanderAmaechi <Alexander.amaechi@live.com>
2022-01-24 05:59:27 +0100

    test for A* heuristic algorithm

--- test_heuristic.py
+++ test_heuristic.py
@@ -5,10 +5,10 @@ from astar import *
 class TestAStar(unittest.TestCase):
 
     def test_heuristic(self):
-        h_test = h((9, 4), (10, 7))
+        h_test = h((5, 4), (10, 7))
         h2_test = h((10, 3), (100, 20))
 
-        self.assertEqual(h_test, 4)
+        self.assertEqual(h_test, 8)
         self.assertEqual(h2_test, 107)
 
 
